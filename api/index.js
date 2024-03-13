ed83f5bc49c136bf778c39dc0af1895ee2fa6a40
Noransaber <noransaber685@gmail.com>
2024-03-13 06:18:31 +0200

    Conneced to the databse

--- api/index.js
+++ api/index.js
@@ -1,4 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!!!');
+  })
+  .catch((err) => {
+    console.error('Could not connect to MongoDB', err);
+  });
 
 const app = express();
 app.listen(3000, () => {
