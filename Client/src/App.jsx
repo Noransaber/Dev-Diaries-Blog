0190fec23f5e0c5f7f7fc58cabe7b1d5326069a9
Noransaber <noransaber685@gmail.com>
2024-03-13 03:51:58 +0200

    Making the header component

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from './pages/About';
@@ -6,11 +7,13 @@ import Dashboard from './pages/Dashboard';
 import Signup from './pages/Signup';
 import Signin from './pages/Signin';
 import Projects from './pages/Projects';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <div>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -1,8 +1,9 @@
+import flowbitePlugin from 'flowbite/plugin';
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', './src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}', 'node_modules/flowbite-react/lib/esm/**/*.js'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [flowbitePlugin],
 };
