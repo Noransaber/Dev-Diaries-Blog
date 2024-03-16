98a7993a71c8aef8306217bcad8d06ff6338a813
Noransaber <noransaber685@gmail.com>
2024-03-17 01:53:16 +0200

    Creating Footer

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -8,6 +8,7 @@ import Signup from './pages/Signup';
 import Signin from './pages/Signin';
 import Projects from './pages/Projects';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
@@ -22,6 +23,7 @@ export default function App() {
           <Route path='/signin' element={<Signin />} />
           <Route path='/projects' element={<Projects />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
