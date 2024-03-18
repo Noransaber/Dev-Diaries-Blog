469d2aaf6dccb3ef4692d1ac8b74e6b844591cd6
Noransaber <noransaber685@gmail.com>
2024-03-18 21:08:57 +0200

    Add toolkit presist

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -4,9 +4,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { Provider } from 'react-redux';
-import store from './redux/store.js';
+import { store } from './redux/store';
+import { persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </PersistGate>
 );
