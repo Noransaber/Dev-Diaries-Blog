195629b48df3b1340b2415406e4895b74dd4ffc0
Noransaber <noransaber685@gmail.com>
2024-03-22 00:18:33 +0200

    Complete sidebar of dashboard

--- Client/src/pages/Dashboard.jsx
+++ Client/src/pages/Dashboard.jsx
@@ -1,10 +1,17 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import DashSidebar from '../components/DashSidebar';
+import DashProfile from '../components/DashProfile';
 
 export default function Dashboard() {
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <div className='md:w-56'>
+        <DashSidebar />
+      </div>
+      <DashProfile />
     </div>
   );
 }
