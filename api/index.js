b18804851e3d903aead84a586921f30f0c265f65
Noransaber <noransaber685@gmail.com>
2024-03-23 22:17:18 +0200

    Create api route for create a post

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import userRouter from './routes/user.router.js';
 import authRouter from './routes/auth.router.js';
 import cookieParser from 'cookie-parser';
+import postRouter from './routes/post.router.js';
 // import errorHandler from './utils/errorHandler.js';
 
 // Load environment variables from .env file
@@ -28,6 +29,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/post', postRouter);
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
---
+++ api/models/post.model.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      max: 500,
+    },
+    title: {
+      type: String,
+      required: true,
+      max: 100,
+    },
+    image: {
+      type: String,
+      default: 'https://www.hostinger.com/tutorials/wp-content/uploads/sites/2/2021/09/how-to-write-a-blog-post.png',
+    },
+    categories: {
+      type: Array,
+      default: 'uncategorized',
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model('Post', postSchema);
+
+export default Post;
