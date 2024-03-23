2f67607674eecb635db70249a29d24533fbac311
Noransaber <noransaber685@gmail.com>
2024-03-23 06:24:59 +0200

    Add delete user api route

--- api/routes/user.router.js
+++ api/routes/user.router.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { test, updateUser } from '../controllers/user.controllers.js';
+import { test, updateUser, deleteUser } from '../controllers/user.controllers.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get('/test', test);
 // Import verifyToken function, to verify user token
 router.put('/update/:userId', verifyToken, updateUser);
+router.delete('/delete/:userId', verifyToken, deleteUser);
 
 export default router;
