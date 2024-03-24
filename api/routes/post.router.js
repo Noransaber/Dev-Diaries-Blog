935ff5c8c94701d0dc34f07920dd25e8497a1400
Noransaber <noransaber685@gmail.com>
2024-03-25 00:05:45 +0200

    Compelete edit post functionality

--- api/routes/post.router.js
+++ api/routes/post.router.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { create } from '../controllers/post.controllers.js';
-import { getposts, deletepost } from '../controllers/post.controllers.js';
+import { getposts, deletepost, updatepost } from '../controllers/post.controllers.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -8,5 +8,6 @@ const router = express.Router();
 router.post('/create', create);
 router.get('/getposts', getposts);
 router.delete('/deletepost/:postId/:userId', verifyToken, deletepost);
+router.delete('/updatepost/:postId/:userId', verifyToken, updatepost);
 
 export default router;
