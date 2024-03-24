1da4690c58185c702632f7d1ada045802d3f0796
Noransaber <noransaber685@gmail.com>
2024-03-24 07:08:57 +0200

    Api route for all posts

--- api/routes/post.router.js
+++ api/routes/post.router.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import { create } from '../controllers/post.controllers.js';
+import { getposts } from '../controllers/post.controllers.js';
 
 const router = express.Router();
 
 router.post('/create', create);
+router.get('/getposts', getposts);
 
 export default router;
