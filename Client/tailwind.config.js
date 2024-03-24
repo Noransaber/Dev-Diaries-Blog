bb5efbc0f0e63ed994755eb985f08699e9f5c1a8
Noransaber <noransaber685@gmail.com>
2024-03-24 07:55:52 +0200

    Show user posts in dashboard

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -1,9 +1,10 @@
 import flowbitePlugin from 'flowbite/plugin';
+import tailwindScrollbar from 'tailwind-scrollbar';
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', './src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}', 'node_modules/flowbite-react/lib/esm/**/*.js'],
   theme: {
     extend: {},
   },
-  plugins: [flowbitePlugin],
+  plugins: [flowbitePlugin, tailwindScrollbar],
 };
