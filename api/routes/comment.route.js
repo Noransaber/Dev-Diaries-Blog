f09204dc78be5bfe76c44ad7a1044b550ff5b34e
Noransaber <noransaber685@gmail.com>
2024-03-27 01:13:15 +0200

    Add comment section to the dashboard

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,10 +1,6 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { createComment } from '../controllers/comment.controllers.js';
-import { getPostComments } from '../controllers/comment.controllers.js';
-import { likeComment } from '../controllers/comment.controllers.js';
-import { editComment } from '../controllers/comment.controllers.js';
-import { deleteComment } from '../controllers/comment.controllers.js';
+import { deleteComment, getcomments, editComment, likeComment, getPostComments, createComment } from '../controllers/comment.controllers.js';
 
 const router = express.Router();
 
@@ -13,5 +9,6 @@ router.get('/getPostComments/:postId', getPostComments);
 router.put('/likeComment/:commentId', verifyToken, likeComment);
 router.put('/editComment/:commentId', verifyToken, editComment);
 router.delete('/deleteComment/:commentId', verifyToken, deleteComment);
+router.get('/getcomments/', verifyToken, getcomments);
 
 export default router;
