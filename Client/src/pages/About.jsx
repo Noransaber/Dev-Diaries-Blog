f3b2879cd57645d4079878354bb51886bbaf0af6
Noransaber <noransaber685@gmail.com>
2024-03-27 08:31:36 +0200

    Content changes

--- Client/src/pages/About.jsx
+++ Client/src/pages/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
         <div>
           <h1 className='text-3xl font font-semibold text-center my-7'>About Dev diaries Blog</h1>
           <div className='text-md text-gray-500 flex flex-col gap-6'>
-            <p>Welcome to Dev diaries Blog! This blog was created by Sahand Ghavidel as a personal project to share his thoughts and ideas with the world. Sahand is a passionate developer who loves to write about technology, coding, and everything in between.</p>
+            <p>Welcome to Dev diaries Blog! This blog was created by Noran as a personal project to share his thoughts and ideas with the world. Sahand is a passionate developer who loves to write about technology, coding, and everything in between.</p>
 
             <p>On this blog, you will find weekly articles and tutorials on topics such as web development, software engineering, and programming languages. Sahand is always learning and exploring new technologies, so be sure to check back often for new content!</p>
 
