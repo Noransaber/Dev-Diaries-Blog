61a0828a677ee1f892003b81948fc1af37c310fa
Noransaber <noransaber685@gmail.com>
2024-03-27 05:20:45 +0200

    Add ashboard overview to the admin dashboard

--- Client/src/pages/Dashboard.jsx
+++ Client/src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import DashProfile from '../components/DashProfile';
 import DashPosts from '../components/DashPost';
 import DashUsers from '../components/DashUsers';
 import DashComments from '../components/DashComments';
+import DashboardComp from '../components/DashboardComp';
 
 export default function Dashboard() {
   const location = useLocation();
@@ -30,6 +31,8 @@ export default function Dashboard() {
       {tab === 'users' && <DashUsers />}
       {/* Comment... */}
       {tab === 'comments' && <DashComments />}
+      {/* Dashboard */}
+      {tab === 'dash' && <DashboardComp />}
     </div>
   );
 }
